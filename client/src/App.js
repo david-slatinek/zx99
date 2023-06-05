d63caddf95da3fcc0b1f12e046cbaa91c043f6c3
Timotej Vošinek <timotej.vosinek@student.um.si>
2023-06-05 09:17:44 +0200

    Dodan header in about page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import ImageUploader from './components/ImageUploader';
 import Facts from './components/Facts';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header';
+import About from './components/About';
+
 
 function App() {
   return (
     <div className='App'>
       {/* <Router> */}
+      <Header/>
       <Routes>
         <Route path='*' Component={ImageUploader} />
         <Route path='/facts/:animal' Component={Facts} />
+        <Route path='/about' Component={About} />
       </Routes>
     {/* </Router> */}
     </div>
---
+++ client/src/components/header.js
@@ -0,0 +1,17 @@
+import './Header.css'
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return(
+        <header className="Header">
+            <a className='naslov'>zx99</a>
+            <div class="header-right">
+                <a><Link to={"/"}>Home</Link></a>
+                <a><Link to={"/about"}>About</Link></a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
